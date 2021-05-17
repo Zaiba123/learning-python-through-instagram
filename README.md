c65a2520905d0892ee102a38068daaec52ea4034
zaiba19 <zaibaiqbal1@gmail.com>
2021-05-16 23:29:20 -0400

    plotted the letter z

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # learning-python-through-instagram
+
+# I take mini tutorials I see on my instagram feed and customize them with my   ideas
---
+++ plot-line.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+#ploted a simple Z for zaiba 
+
+
+x = [0,2,0,2]
+y = [4,4,1,1]
+
+plt.plot(x,y)
+plt.show()
